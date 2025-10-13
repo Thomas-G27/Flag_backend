7d299e2c247479480157c7cf734230c020717a3e
EmmanuelMDG <mdg.m.emmanuel@gmail.com>
2025-10-13 10:34:25 +0200

    Correction DAO

--- src/main/java/com/takima/backskeleton/DAO/PaysDao.java
+++ src/main/java/com/takima/backskeleton/DAO/PaysDao.java
@@ -4,19 +4,35 @@ import com.takima.backskeleton.models.Pays;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface PaysDao extends JpaRepository<Pays,Long> {
-    List<Pays> findDistinctByLanguages_NameIgnoreCase(String name);
+@Repository
+public interface PaysDao extends JpaRepository<Pays, Long> {
+
+
+    List<Pays> findDistinctByLanguages_NameIgnoreCase(String languageName);
+
+
     List<Pays> findByContinent_NameIgnoreCase(String continentName);
-    List<Pays> findByContinentIgnoreCase(String continent);
+
+
+    List<Pays> findByLanguages_Id(Long languageId);
+    List<Pays> findByContinent_Id(Long continentId);
+
+
     @Query("""
-select distinct p
-  from Pays p
-  join p.languages l
-  where lower(l.name) = lower(:lang)
-    and lower(p.continent.name) = lower(:continent)
-""")
-    List<Pays> findByLanguageAndContinent(@Param("lang") String lang,
-                                          @Param("continent") String continent);}
+         select distinct p
+         from Pays p
+         join p.languages l
+         where lower(l.name) = lower(:language)
+           and lower(p.continent.name) = lower(:continent)
+         """)
+    List<Pays> findByLanguageAndContinent(@Param("language") String language,
+                                          @Param("continent") String continent);
+
+
+    Optional<Pays> findByNameIgnoreCase(String name);
+}
