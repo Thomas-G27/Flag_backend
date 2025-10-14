9892afdc3edcc143fd846cea1d0e921e4d1b3430
ThomasG <thomas.gbt2003@gmail.com>
2025-10-14 11:27:12 +0200

    ajustement de l'initialisation du back

--- src/main/java/com/takima/backskeleton/BackSkeletonApplication.java
+++ src/main/java/com/takima/backskeleton/BackSkeletonApplication.java
@@ -1,7 +1,10 @@
 package com.takima.backskeleton;
 
+import com.takima.backskeleton.services.PaysService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackSkeletonApplication {
@@ -9,5 +12,10 @@ public class BackSkeletonApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackSkeletonApplication.class, args);
 	}
-
+		@Bean
+		CommandLineRunner seedOnStart(PaysService paysService) {
+			return args -> {
+				paysService.remplirPaysETContinent();
+			};
+		}
 }
